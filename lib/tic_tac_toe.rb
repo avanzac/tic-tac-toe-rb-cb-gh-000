255e98e1bfcf992e4cf89e876f8e2b1f00e9697d
darren mcgregor <darren.mcgregor@avanzac.co.uk>
2018-08-22 13:09:50 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -6,9 +6,13 @@ def display_board(board)
    puts " #{board[6]} | #{board[7]} | #{board[8]} "
  end
 
- def input_to_index(user_input)
+def input_to_index(user_input)
       user_input.to_i - 1
     end
+
+def move(board, index, player)
+       board[index] = player
+     end
 
 # Helper Method
 def position_taken?(board, index)
