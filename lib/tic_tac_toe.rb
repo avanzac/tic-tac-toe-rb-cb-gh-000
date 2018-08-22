836d9caa5f5ff5a0d04aed560fed69835de9d5b3
darren mcgregor <darren.mcgregor@avanzac.co.uk>
2018-08-22 13:07:42 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,5 +1,14 @@
+def display_board(board)
+   puts " #{board[0]} | #{board[1]} | #{board[2]} "
+   puts "-----------"
+   puts " #{board[3]} | #{board[4]} | #{board[5]} "
+   puts "-----------"
+   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+ end
 
-
+ def input_to_index(user_input)
+      user_input.to_i - 1
+    end
 
 # Helper Method
 def position_taken?(board, index)
