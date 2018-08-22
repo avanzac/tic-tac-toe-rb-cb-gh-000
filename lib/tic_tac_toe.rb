b410e7033135977c0b5b899d93bfeb70795811a3
darren mcgregor <darren.mcgregor@avanzac.co.uk>
2018-08-22 13:16:12 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -41,7 +41,7 @@ def current_player(board)
 
 def turn(board)
   puts "Please enter 1-9:"
-  user_input = gets.chomp     
+  user_input = gets.chomp
   index = input_to_index(user_input)
   if valid_move?(board, index)
      move(board, index)
